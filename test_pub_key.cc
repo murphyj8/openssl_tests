0e2f3420ace2d48a4c97f35024bda9f3d1e17c93
John Murphy <j.murphy@nchain.com>
2023-01-17 12:18:25 +0000

    Updated the tests to convert between compressed/uncompressed public keys

--- test_pub_key.cc
+++ test_pub_key.cc
@@ -11,7 +11,6 @@
 
 int main(int argc, char* argv[]){
     std::cout << "Starting" << std::endl; 
-
     EC_GROUP * m_gp = EC_GROUP_new_by_curve_name(NID_secp384r1);
     EC_POINT * m_ec = EC_POINT_new(m_gp);
 
@@ -71,12 +70,60 @@ int main(int argc, char* argv[]){
     // back to an EC-point
     const EC_POINT * ec_point_pk = EC_KEY_get0_public_key(ec_tmp_ptr);
 
-    char *ecChar = EC_POINT_point2hex(m_gp, ec_point_pk, POINT_CONVERSION_COMPRESSED, ctxptr.get());
+    char *ecChar = EC_POINT_point2hex(m_gp, ec_point_pk, POINT_CONVERSION_UNCOMPRESSED, ctxptr.get());
 
     std::string second_key(ecChar);
     std::cout << second_key << std::endl; 
     assert (second_key == hexStr);
+
     OPENSSL_free(ecChar);
+
+
+    // compressed or uncompressed?
+    {
+        std::string compressedKey ("0373D23D4E1D88903DD7DFCF0F12E842AF8AE30DB58FAE6F79A4CF8271CBE636BF");
+        std::cout << "Compressed PubKey input -> " << compressedKey << std::endl; 
+         // Allocate for CTX
+        std::unique_ptr<BN_CTX, decltype(&BN_CTX_free)> ctxptr (BN_CTX_new(), &BN_CTX_free );
+        assert(ctxptr != nullptr);
+
+        EC_KEY_ptr ec_key (EC_KEY_new(), &EC_KEY_free); 
+        EC_GROUP *ec_group = EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1);
+        EC_KEY_set_group(ec_key.get(), ec_group);
+        EC_KEY_set_asn1_flag(ec_key.get(), OPENSSL_EC_NAMED_CURVE);
+
+        EC_POINT * m_ec = EC_POINT_new(ec_group);
+        m_ec = EC_POINT_hex2point(ec_group, compressedKey.c_str(), m_ec, ctxptr.get());
+        assert(m_ec != nullptr);
+
+        if(EC_KEY_set_public_key(ec_key.get(), m_ec) <= 0){
+            std::cout << "Could not assign pub key" << std::endl;
+            return -1;
+        }
+
+        EVP_PKEY * pkey = EVP_PKEY_new(); 
+        assert(pkey != nullptr);
+        if(EVP_PKEY_assign_EC_KEY(pkey, ec_key.get()) <= 0){
+            std::cout << "Failure to asign to PK" << std::endl; 
+            return -1; 
+        }
+
+        //back to an ec key
+        const EC_KEY * ec_tmp_ptr = EVP_PKEY_get0_EC_KEY(pkey);
+        assert(ec_tmp_ptr != nullptr);
+
+        // back to an EC-point
+        const EC_POINT * ec_point_pk = EC_KEY_get0_public_key(ec_tmp_ptr);
+
+        //char *ecChar = EC_POINT_point2hex(ec_group, ec_point_pk, POINT_CONVERSION_COMPRESSED, ctxptr.get());
+        std::string compressed_pub_key = EC_POINT_point2hex(ec_group, ec_point_pk, POINT_CONVERSION_COMPRESSED, ctxptr.get());
+        assert (compressed_pub_key == compressedKey); 
+
+        std::string uncompressed_pub_key = EC_POINT_point2hex(ec_group, ec_point_pk, POINT_CONVERSION_UNCOMPRESSED, ctxptr.get());
+
+        std::cout << "Uncompressed key -> " << uncompressed_pub_key << std::endl; 
+    }
+
     std::cout << "Ending" << std::endl; 
     
 }
